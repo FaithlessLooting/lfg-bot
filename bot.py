32b4117376b718930b01d52311ea63c16f61313c
FaithlessLooting <matthew.dove13@gmail.com>
2020-06-01 01:38:59 +0100

    Initial working model
    Signed-off-by: FaithlessLooting <matthew.dove13@gmail.com>

--- bot.py
+++ bot.py
@@ -1,16 +1,36 @@
-# bot.py
+#!/usr/bin/env python
+
+# bot.py: discord bot for organising games of MTG via discord servers
+__author__ = "Matthe Dove"
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Rob Knight"
+__email__ = "matthew.dove13@gmail.com"
+__status__ = "Development"
+
 import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
+
 load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+bot = commands.Bot(command_prefix='!')
+lfg_fightqueue = []
+@bot.command(name='lfg', help='Adds you too the fight queue')
+async def look_to_fight(ctx):
+    if not lfg_fightqueue:
+        lfg_fightqueue.append(ctx.message.author.mention)
+        queuedmessage = "Thanks {}, you have been added to the queue.".format(lfg_fightqueue[0])
+        response = queuedmessage
+    elif len(lfg_fightqueue) == 1:
+        lfg_fightqueue.append(ctx.message.author.mention)
+        fightmessage = "{} and {}... FIGHT.".format(lfg_fightqueue[0], lfg_fightqueue[1])
+        response = fightmessage
+        lfg_fightqueue.clear()
+        
+    await ctx.send(response)
 
-TOKEN = os.getenv("DISCORD_TOKEN")
-print (TOKEN)
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
-client.run(TOKEN)
+bot.run(TOKEN)
