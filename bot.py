3eba59ba5cc1a14bbb1944c6aac636a8a4a27df8
FaithlessLooting <matthew.dove13@gmail.com>
2020-06-03 18:55:25 +0100

    Leagues! first draft of much anticipated feature
    Signed-off-by: FaithlessLooting <matthew.dove13@gmail.com>

--- bot.py
+++ bot.py
@@ -22,6 +22,8 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 bot = commands.Bot(command_prefix='!')
 lfg_fightqueue = []
+lfg_leaguequeue = []
+current_fights = []
 @bot.command(name='lfg', help='Adds you too the fight queue')
 async def look_to_fight(ctx):
     if not lfg_fightqueue:
@@ -87,4 +89,55 @@ async def joinleaguequeue(ctx):
         User_ID, User_Name, Score = row
         print('{} {} {}'.format(
             User_ID, User_Name, Score))
+    if not lfg_leaguequeue:
+        lfg_leaguequeue.append(ctx.message.author.mention)
+        queuedmessage = "Thanks {}, you have been added to the queue.".format(lfg_leaguequeue[0])
+        response = queuedmessage
+    elif len(lfg_leaguequeue) == 1:
+        lfg_leaguequeue.append(ctx.message.author.mention)
+        fightmessage = "{} and {}... FIGHT. Please report your result by the victor typing !winner, currently this is an honor based system, please message a mod if you feel a player is cheating".format(lfg_leaguequeue[0], lfg_leaguequeue[1])
+        current_fights.append(lfg_leaguequeue[0]+"-"+lfg_leaguequeue[1])
+        response = fightmessage
+        lfg_leaguequeue.clear()
+        print(current_fights[0])
+    await ctx.send(response)
+@bot.command(name='winner', help='reports league winner')
+async def winner(ctx):
+    if current_fights:
+        conn = sqlite3.connect('lfgbot.db')  
+        c = conn.cursor()
+        author=ctx.message.author.mention
+        authorid=ctx.message.author.id
+        authorid=str(authorid)
+        author=str(author)
+        c.execute("select User_ID, User_Name, Score from users where User_ID = '"+authorid+"'")
+        for row in c.fetchall():
+            User_ID, User_Name, Score = row
+            print('{} {} {}'.format(
+                User_ID, User_Name, Score))
+            OldScore = Score
+            print(OldScore)
+        c.close()
+        for i in current_fights:
+            if authorid in i:
+                NewScore= OldScore+3
+                NewScore = str(NewScore)
+                print(NewScore)
+                print(authorid)
+                conn = sqlite3.connect('lfgbot.db')  
+                c = conn.cursor()
+                c.execute("UPDATE users SET Score = "+NewScore+" WHERE User_ID = "+authorid)
+                conn.commit()
+                current_fights.remove(i)
+                c.close()
+                queuedmessage = "Thanks {}, your result has been confirmed.".format(ctx.message.author.mention)
+                response = queuedmessage
+            else:
+                queuedmessage = "Sorry {}, no matches found for you.".format(ctx.message.author.mention)
+                response = queuedmessage
+    else:
+         queuedmessage = "Error: Queue Empty"
+         response = queuedmessage
+    await ctx.send(response)
+                
 bot.run(TOKEN)
