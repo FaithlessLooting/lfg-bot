c21c7b32a2f555df69b7e650c7fada91bcfbd11d
FaithlessLooting <matthew.dove13@gmail.com>
2020-06-03 17:43:02 +0100

    add users to database
    Signed-off-by: FaithlessLooting <matthew.dove13@gmail.com>

--- bot.py
+++ bot.py
@@ -9,10 +9,13 @@ __email__ = "matthew.dove13@gmail.com"
 __status__ = "Development"
 
 import os
-
+import re
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+import sqlite3
+import pandas as pd
+from pandas import DataFrame
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -34,7 +37,7 @@ async def look_to_fight(ctx):
     await ctx.send(response)
 
 @bot.command(name='leave', help='Leaves the queue')
-async def look_to_fight(ctx):
+async def leave_fight(ctx):
     if ctx.message.author.mention in lfg_fightqueue:
         
         queuedmessage = "Thanks {}, you have been removed from.".format(lfg_fightqueue[0])
@@ -45,5 +48,49 @@ async def look_to_fight(ctx):
         response = fightmessage
         
     await ctx.send(response)
+    
+def add_user(conn, user):
+    """
+    Create a new task
+    :param conn:
+    :param task:
+    :return:
+    """
+
+    sql = ''' INSERT INTO USERS(User_ID,User_Name,Score)
+              VALUES(?,?,?) '''
+    conn = sqlite3.connect('lfgbot.db')
+    print(user)
+    print(sql)
+    c = conn.cursor()
+    c.execute(sql, user)
+    return c.lastrowid
+
+
+@bot.command(name='joinleague', help='Joins Monthly League')
+async def joinleague(ctx):
+        conn = sqlite3.connect('lfgbot.db')  
+        c = conn.cursor()
+        author=ctx.message.author.mention
+        author = re.sub('[<>]', '', author)
+        authorid=ctx.message.author.id
+        print(author)
+        print(authorid)
+        authorid=str(authorid)
+        author=str(author)
+        # Insert a row of data
+        c.execute("INSERT INTO users VALUES ("+authorid+",'"+author+"',"+"0"+")")
+
+        # Save (commit) the changes
+        conn.commit()
+
+        # We can also close the connection if we are done with it.
+        # Just be sure any changes have been committed or they will be lost.
+        conn.close()
+        queuedmessage = "Thanks {}, you have been registered for the monthly league.".format(ctx.message.author.mention)
+        response = queuedmessage
+        await ctx.send(response)
+
+
 
 bot.run(TOKEN)
--- db.py
+++ db.py
@@ -1,19 +1,14 @@
 import sqlite3
 from sqlite3 import Error
 
+conn = sqlite3.connect('lfgbot.db')  # You can create a new database by changing the name within the quotes
+c = conn.cursor() # The database will be saved in the location where your 'py' file is saved
 
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
+# Create table - USERS
+c.execute('''CREATE TABLE USERS
+             ([User_ID] INTEGER PRIMARY KEY,[User_Name] text, [Score] integer)''')                 
+conn.commit()
 
-
-if __name__ == '__main__':
-    create_connection(r"E:\Python\lfgbot\lfgbot.db")
+# Note that the syntax to create new tables should only be used once in the code (unless you dropped the table/s at the end of the code). 
+# The [generated_id] column is used to set an auto-increment ID for each record
+# When creating a new table, you can add both the field names as well as the field formats (e.g., Text)
